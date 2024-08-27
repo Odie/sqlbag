3cece1239dc5a77163a241126bdbef537d30e4c4
Jonathan Shieh <jonathan.shieh@gmail.com>
2024-08-27 09:55:04 +0800

    Update version number to match latest on PyPI

--- setup.py
+++ setup.py
@@ -9,16 +9,13 @@ with io.open("README.rst") as f:
 
 setup(
     name="sqlbag",
-    version="0.1.1579049654",
+    version="0.1.1617247075",
     url="https://github.com/djrobstep/sqlbag",
     description="various snippets of SQL-related boilerplate",
     long_description=readme,
     author="Robert Lechte",
     author_email="robertlechte@gmail.com",
-    install_requires=[
-        "pathlib; python_version<'3'",
-        "six",
-        "sqlalchemy"],
+    install_requires=["pathlib; python_version<'3'", "six", "sqlalchemy"],
     zip_safe=False,
     packages=find_packages(),
     classifiers=["Development Status :: 3 - Alpha"],
